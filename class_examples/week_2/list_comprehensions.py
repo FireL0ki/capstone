455a48a4a8f2922eb3823a4971eeee3fd3fef29d
muell <muell758@d.umn.edu>
2022-09-15 15:35:08 -0500

    add examples to list_comprehensions

--- class_examples/week_2/list_comprehensions.py
+++ class_examples/week_2/list_comprehensions.py
@@ -17,3 +17,31 @@ print(temp_celsius)
 
 average = sum(temp_celsius) / len(temp_celsius)
 print(f'The average i9s {average:.2f}')  # .2f -- decimal place formatting
+
+# numbers -- start with list [2, 4, 6], write a comprehension to make a new list with each number increased by one
+even_numbers = [2, 4, 6]
+print(even_numbers)
+
+increased_numbers = [ number + 1 for number in even_numbers]
+print(increased_numbers)
+
+# create new list with non-zero numbers
+number_list = [0, 3, 4, 0, 22, 1]
+print(number_list)
+
+non_zero_numbers = [ number for number in number_list if number != 0]
+print(non_zero_numbers)
+
+# create new list with non-itec classes
+classes_list = [ 'ITEC 2560', 'BTEC 1010', 'ITEC 2905']
+print(classes_list)
+
+non_itec_classes = [ c for c in classes_list if not c.startswith('ITEC') ]
+print(non_itec_classes)
+
+# create new list where numbers are doubled, only if not zero
+all_numbers_list = [0, 10, 4, 0, 32]
+print(all_numbers_list)
+
+doubled_numbers = [ number * 2 for number in all_numbers_list if number != 0]
+print(doubled_numbers)
