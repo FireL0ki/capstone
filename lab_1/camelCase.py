fbc2e3c8dd5b1e0d2e2f3904c2d84100c2e3d29e
Sif Oberon <muell758@d.umn.edu>
2022-09-16 17:49:19 -0500

    Add banner welcome message to start of program

--- lab_1/camelCase.py
+++ lab_1/camelCase.py
@@ -2,8 +2,17 @@
 
 user_input = str(input('Enter a sentence to convert to camelCase '))
 
+def banner():
+    """ Display program name """ # triple quotes are doc strings
+    message = 'Awesome camelCase Program!'
+    stars = '*' * len(message)
+    print(f'\n{stars} \n{message} \n{stars}\n')
+
+banner()
+
 # defines a function that converts the sentence to title case, and gets rid of spaces
 def camel_case(x):
+
     # .title() converts the phrase to title case (first letter capitalized of each word), then .replace() replaces all " " spaces with no space 
     x = x.title().replace(" ", "")
     # Then x[0] grabs the first (0th) letter and makes it lowercase, and then we need to grab the rest of the word [1:] (from the second spot onward) and concatonate it to the first letter
