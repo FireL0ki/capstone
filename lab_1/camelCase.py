57b41dc244d7873453322c54399f2b5aa7965a1f
Sif Oberon <muell758@d.umn.edu>
2022-09-16 17:55:19 -0500

    Print instructions for user

--- lab_1/camelCase.py
+++ lab_1/camelCase.py
@@ -1,18 +1,24 @@
 # program to convert a sentence into camel case
 
-user_input = str(input('Enter a sentence to convert to camelCase '))
-
 def banner():
     """ Display program name """ # triple quotes are doc strings
     message = 'Awesome camelCase Program!'
     stars = '*' * len(message)
     print(f'\n{stars} \n{message} \n{stars}\n')
 
-banner()
+def instructions():
+    print('Enter a sentence and this program will convert it to camelCase.')
+
+def main():
+    banner()
+    instructions()
+
+main()
+
+user_input = str(input('Enter a sentence to convert to camelCase '))
 
 # defines a function that converts the sentence to title case, and gets rid of spaces
 def camel_case(x):
-
     # .title() converts the phrase to title case (first letter capitalized of each word), then .replace() replaces all " " spaces with no space 
     x = x.title().replace(" ", "")
     # Then x[0] grabs the first (0th) letter and makes it lowercase, and then we need to grab the rest of the word [1:] (from the second spot onward) and concatonate it to the first letter
