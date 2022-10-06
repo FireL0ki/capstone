7fd741f998f82cb9516aaed564dab671d9079056
Sif Oberon <muell758@d.umn.edu>
2022-10-05 19:25:01 -0500

    lab_4 update

--- lab_4/test_magic.py
+++ lab_4/test_magic.py
@@ -1,3 +1,6 @@
+import functions_magic
+from unittest import TestCase
+
 """ TODO create a test case to test the following functions,
 
 generate_url_for_question
@@ -14,3 +17,27 @@ extract_answer_from_response
  of your ideas now. We'll talk about ways to test other aspects of this program in class.
 
 """
+class TestFunctionsMagic(TestCase):
+
+    def test_generate_url_for_question(self):
+        # check that expected URL is returned for an example question
+
+        #  https://8ball.delegator.com/magic/JSON/ 
+        example_question = 'Will I ever love again?'
+        self.assertEqual('https://8ball.delegator.com/magic/JSON/Will I ever love again?', functions_magic.generate_url_for_question(example_question))
+
+
+    def test_extract_answer_from_response(self):
+        # create example dictionaries that match expected API response, check that the correct answer is extracted and returned
+
+        magic_dictionary = {
+            'question': 'Will it be sunny tomorrow?', 
+            'answer': 'Yes', 
+            'type': 'Affirmative'
+            }
+
+        self.assertEqual('Yes', functions_magic.extract_answer_from_response(magic_dictionary))
+
+        # create example dictionaries with a different structure to the one returned from the API, check that the function returns None
+        new_structure_dictionary = {}
+
