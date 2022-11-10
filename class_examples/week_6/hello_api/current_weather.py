93ec46ae8dc69a54221e57f970ab33d1d50905eb
Sif Oberon <muell758@d.umn.edu>
2022-11-10 17:48:33 -0600

    fix key exposure

--- class_examples/week_6/hello_api/current_weather.py
+++ class_examples/week_6/hello_api/current_weather.py
@@ -1,9 +1,21 @@
 import requests
 from pprint import pprint
+import os
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q=minneapolis,mn,us&units=imperial&appid=fb94f018c382e75efcfc009a97284108'
+# TODO this key works, but for some reason the program is unable to grab it from system environment variables
+key = os.environ.get('WEATHER_KEY')
+print(key)
 
-data = requests.get(url).json()
+user_country = input('Enter the two letter code of a country: ')
+user_city = input('Enter the name of a city in that country: ')
+
+user_city_country = f'{user_city},{user_country}'
+
+url = f'https://api.openweathermap.org/data/2.5/weather'
+query = {'q': user_city_country, 'units': 'celsius', 'appid': key} # to simplify url, and control individual variables in the query more easily
+
+
+data = requests.get(url, params=query).json()
 
 pprint(data)
 
